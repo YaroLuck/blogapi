d95ec7ca41cf48b078a22d995cb69f91d5df81c7
Admin <openjaroslav@gmail.com>
2020-07-17 10:26:58 +1000

    Initial commit

---
+++ posts/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
+from posts.models import Post
+from posts.serializers import PostSerializer
+
+
+class PostList(ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
